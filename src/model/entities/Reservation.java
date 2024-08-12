4e3ec4c8d863813f30b77240bee823b398f6bc8e
Mateus <mateus.olvr2002@gmail.com>
2024-08-12 17:42:40 -0300

    Muito ruim

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -1,17 +1,17 @@
 package model.entities;
 
-import java.text.SimpleDateFormat;
 import java.time.Duration;
-import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Reservation {
 	private Integer roomNumber;
-	private LocalDateTime checkIn;
-	private LocalDateTime checkOut;
+	private LocalDate checkIn;
+	private LocalDate checkOut;
 	
-	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/mm/yyyy");
+	private static DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 	
-	public Reservation(Integer roomNumber, LocalDateTime chekin, LocalDateTime chekout) {
+	public Reservation(Integer roomNumber, LocalDate chekin, LocalDate chekout) {
 		this.roomNumber = roomNumber;
 		this.checkIn = chekin;
 		this.checkOut = chekout;
@@ -25,11 +25,11 @@ public class Reservation {
 		this.roomNumber = roomNumber;
 	}
 
-	public LocalDateTime getChekin() {
+	public LocalDate getChekin() {
 		return checkIn;
 	}
 
-	public LocalDateTime getChekout() {
+	public LocalDate getChekout() {
 		return checkOut;
 	}
 	
@@ -38,7 +38,7 @@ public class Reservation {
 		return diff.toDays();
 	}
 	
-	public void updateDates(LocalDateTime checkIn, LocalDateTime checkOut) {
+	public void updateDates(LocalDate checkIn, LocalDate checkOut) {
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
 	}
@@ -49,9 +49,9 @@ public class Reservation {
 		builder.append("Room ");
 		builder.append(roomNumber);
 		builder.append(", checkIn: ");
-		builder.append(sdf.format(checkIn));
+		builder.append(fmt.format(checkIn));
 		builder.append(", checkOut: ");
-		builder.append(sdf.format(checkOut));
+		builder.append(fmt.format(checkOut));
 		builder.append(", ");
 		builder.append(duration());
 		builder.append(" nights");
