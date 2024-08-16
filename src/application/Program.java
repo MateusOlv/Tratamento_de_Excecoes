7e16b92659abdfc691ef456aedf4e65a7afc7713
Mateus <mateus.olvr2002@gmail.com>
2024-08-16 14:06:17 -0300

    Versão boa e correta de tratamento de exceções

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,30 +1,29 @@
 package application;
 
-import java.text.ParseException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.Scanner;
 
 import model.entities.Reservation;
+import model.exceptions.DomainException;
 
 public class Program {
 
-	public static void main(String[] args) throws ParseException{
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
-		System.out.print("Room number: ");
-		Integer roomNumber = sc.nextInt();
-		
-		System.out.print("Check-in date (dd/MM/yyyy): ");
-		LocalDate checkIn = LocalDate.parse(sc.next(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
-		
-		System.out.print("Check-out date (dd/MM/yyyy): ");
-		LocalDate checkOut = LocalDate.parse(sc.next(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
-		
-		if(!checkOut.isAfter(checkIn)) {
-			System.out.println("Error in reservation: Check-out date must be after check-in date");
-		}
-		else {
+		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+			
+		try {
+			System.out.print("Room number: ");
+			Integer roomNumber = sc.nextInt();
+			
+			System.out.print("Check-in date (dd/MM/yyyy): ");
+			LocalDate checkIn = LocalDate.parse(sc.next(), fmt);
+			
+			System.out.print("Check-out date (dd/MM/yyyy): ");
+			LocalDate checkOut = LocalDate.parse(sc.next(), fmt);
+			
 			Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);
 			System.out.println("Reservation: " + reservation);
 			
@@ -32,19 +31,23 @@ public class Program {
 			System.out.println("Enter data to update the reservation:");
 	
 			System.out.print("Check-in date (dd/MM/yyyy): ");
-			checkIn = LocalDate.parse(sc.next(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+			checkIn = LocalDate.parse(sc.next(), fmt);
 			
 			System.out.print("Check-out date (dd/MM/yyyy): ");
-			checkOut = LocalDate.parse(sc.next(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+			checkOut = LocalDate.parse(sc.next(), fmt);
 			
-			String error = reservation.updateDates(checkIn, checkOut);
-			if(error != null) {
-				System.out.println("Error in reservation: " + error);
-			}
-			else {
-				System.out.println("Reservation: " + reservation);
-			}
+			reservation.updateDates(checkIn, checkOut);
+			System.out.println("Reservation: " + reservation);
 			
+		}
+		catch (DateTimeParseException e){
+			System.out.println("Invalid date format!");
+		}
+		catch (DomainException e) {
+			System.out.println("Error in reservation: " + e.getMessage());
+		}
+		catch (RuntimeException e) {
+			System.out.println("Unexpected error!");
 		}
 					
 		sc.close();
